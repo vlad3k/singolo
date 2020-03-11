e5974dfe2f1bb2b61e95099543297746801bcc81
vlad3k <vlad3k@bk.ru>
2020-03-12 00:51:59 +0300

    fix: smooth scroll from links menu to section

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-const navLinks = document.querySelectorAll('.nav__link');
+document.addEventListener('DOMContentLoaded', function(){
 
-navLinks.forEach((link) => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (e.target.classList.contains('nav__link--active')) return;
-    navLinks.forEach(link => link.classList.remove('nav__link--active'));
-    const linkTo = document.querySelector(e.target.getAttribute('href')).offsetTop - 95;
-    e.target.classList.add('nav__link--active');
-    window.scroll({
-      top: linkTo,
-      left: 0,
-      behavior: 'smooth'
-    });
-  })
+  const navLinks = document.querySelectorAll('.nav__link');
+  const sections = document.querySelectorAll('section');
+  const sectionMargin = 300;
+
+  const makeActive = (link) => navLinks[link].classList.add('nav__link--active');
+  const removeActive = (link) => navLinks[link].classList.remove('nav__link--active');
+  const removeAllActive = () => [...Array(sections.length).keys()].forEach((link) => removeActive(link));
+
+  let currentActive = 0;
+
+  window.addEventListener('scroll', () => {
+    const current = sections.length - [...sections].reverse().findIndex((section) => window.scrollY >= section.offsetTop - sectionMargin ) - 1;
+    if (current !== currentActive) {
+      removeAllActive();
+      currentActive = current;
+      makeActive(current);
+    }
+  });
+
 });
